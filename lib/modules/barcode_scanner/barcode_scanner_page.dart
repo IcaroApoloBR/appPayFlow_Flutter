31824401151fa2a1e725c22de944c433a3786ef1
Icaro Apolo <apoloraci@gmail.com>
2022-05-27 19:40:01 -0300

    style(barcode scanner): create bottom sheet widget and style page

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -4,6 +4,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
 import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
@@ -18,6 +19,14 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
+    return BottomSheetWidget(
+      title: "Não foi possível identificar um código de barras.",
+      subtitle: "Tente escanear novamente ou digite o código do seu boleto.",
+      primaryLabel: "Escanear novamente",
+      primaryOnPressed: () {},
+      secondaryLabel: "Digitar código",
+      secondaryOnPressed: () {},
+    );
     return SafeArea(
       top: true,
       bottom: true,
