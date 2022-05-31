8299991e90b736029dc5270efc81c070a4fe579d
Icaro Apolo <apoloraci@gmail.com>
2022-05-31 18:59:54 -0300

    fix(bugs in barcode): fix errors in camera barcode scanner

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
 
 import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
@@ -18,15 +19,17 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Pay Flow',
-        theme: ThemeData(
-            primarySwatch: Colors.green, primaryColor: AppColors.primary),
-        initialRoute: "/splash",
-        routes: {
-          "/splash": (context) => SplashPage(),
-          "/home": (context) => HomePage(),
-          "/login": (context) => LoginPage(),
-          "/barcode_scanner": (context) => BarcodeScannerPage()
-        });
+      title: 'Pay Flow',
+      theme: ThemeData(
+          primarySwatch: Colors.green, primaryColor: AppColors.primary),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage()
+      },
+    );
   }
 }
