72d52e671ad2af54a1d4abb6d66aa2fdcdadb314
Icaro Apolo <apoloraci@gmail.com>
2022-06-04 14:09:56 -0300

    fix(boleto page): create boleto info widget and finish boleto page

--- lib/modules/meus_boletos/meus_boletos_page.dart
+++ lib/modules/meus_boletos/meus_boletos_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/boleto_model.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/boleto_info/boleto_info_widget.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_controller.dart';
 import 'package:payflow/shared/widgets/boleto_list/boleto_list_widget.dart';
 
 class MeusBoletosPage extends StatefulWidget {
@@ -11,12 +14,31 @@ class MeusBoletosPage extends StatefulWidget {
 }
 
 class _MeusBoletosPageState extends State<MeusBoletosPage> {
+  final controller = BoletoListController();
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        Stack(
+          children: [
+            Container(
+              color: AppColors.primary,
+              height: 40,
+              width: double.maxFinite,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: ValueListenableBuilder<List<BoletoModel>>(
+                valueListenable: controller.boletosNotifier,
+                builder: (_, boletos, __) =>
+                    BoletoInfoWidget(size: boletos.length),
+              ),
+            ),
+          ],
+        ),
         Padding(
-          padding: const EdgeInsets.only(top: 24),
+          padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
           child: Row(
             children: [
               Text(
@@ -27,12 +49,17 @@ class _MeusBoletosPageState extends State<MeusBoletosPage> {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24),
+          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
           child: Divider(
             color: AppColors.stroke,
           ),
         ),
-        BoletoListWidget()
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: BoletoListWidget(
+            controller: controller,
+          ),
+        )
       ],
     );
   }
